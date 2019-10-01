d615a62034f438ccfbda88e8e5658f9c4b132f01
Victor Akanbi <vakanbi971@west-mec.org>
2019-10-01 15:19:53 -0700

    Update code based on directions
    Updated code

---
+++ Exercises/read-demo.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+fs.readFile('./data1.json', 'utf-8', function(err, data) {
+    console.log(data);
+    console.log(data.name);
+    data = JSON.parse(data);
+    console.log(data.name);
+});
+
+
